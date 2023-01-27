592a1b8600c47ec12cf498bb2557183cafa46678
mwasifraza <mwasifraza95@gmail.com>
2023-01-27 12:59:33 +0500

    list book with category

--- app/Http/Controllers/UserDashboardController.php
+++ app/Http/Controllers/UserDashboardController.php
@@ -4,14 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Book;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserDashboardController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
+        if(isset($request->cat)){
+            $books = Book::where('category', $request->cat)->paginate(5);
+        }else{
+            $books = Book::paginate(5);
+        }
         return view('dashboard', [
-            'books' => Book::paginate(5),
+            'books' => $books,
+            'categories' => Category::all(),
         
         ]);
     }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,42 +18,69 @@ User Dashboard | Book Store
         </div>
     </div>
     <div class="col-sm-8 p-3">
-        @foreach ($books as $book)
-        <div class="card mb-3">
-            <div class="card-body bg-light d-flex">
-                <div class="bg-white" style="width: 180px; height:180px;">
-                    <img src="{{ asset($book->cover_photo) }}" alt="book cover" class="w-100">
+        @if (isset($categories[0]))
+            <nav class="navbar navbar-expand-lg bg-light navbar-light mb-4">
+                <div class="container-fluid">
+                    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                        <ul class="navbar-nav">
+                            @foreach ($categories as $category)
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('user.dashboard', ['cat' => $category->id]) }}">
+                                        {{ $category->category_name }}
+                                    </a>
+                                </li>                            
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
-                <div class="ms-3 w-75">
-                    <h4 class="m-0">{{ $book->title }}</h4>
-                    <div class="h6">
-                        <span class="me-2">
-                            <i class="fa-solid fa-tag text-muted"></i>&nbsp; 
-                            <a href="" class="text-decoration-none text-muted fw-normal">{{ $book->category_info->category_name }}</a>
-                        </span>
-                        <span class="me-2">
-                            <i class="fa-solid fa-user text-muted"></i>&nbsp;
-                            <a href="" class="text-decoration-none text-muted fw-normal">{{ $book->author }}</a>
-                        </span>
-                        <span class="me-2">
-                            <i class="fa-solid fa-calendar text-muted"></i>&nbsp;
-                            <a href="" class="text-decoration-none text-muted fw-normal">{{ $book->created_at->format("d-m-Y") }}</a>
-                        </span>
-                    </div>
-                    <div class="mt-3">
-                        <p>{{ substr($book->description, 0 , 200)."..." }}</p>
-                    </div>
-                    <div class="d-flex justify-content-between">
-                        <h5 class="text-muted">Rs. {{ $book->price }}</h5>
-                        <div>
-                            <button class="btn btn-secondary btn-sm px-4">Buy Now</button>
-                            <button class="btn btn-secondary btn-sm px-4 ms-2">Read More</button>
+            </nav>
+        @endif
+
+        @if (isset($books[0]))
+            @foreach ($books as $book)
+                <div class="card mb-3">
+                    <div class="card-body bg-light d-flex">
+                        <div class="bg-white d-flex justify-content-center align-items-center" style="width: 180px; height:180px;">
+                            <img src="{{ asset($book->cover_photo) }}" alt="book cover" class="w-100">
+                        </div>
+                        <div class="ms-3 w-75">
+                            <h4 class="m-0">{{ $book->title }}</h4>
+                            <div class="h6">
+                                <span class="me-2">
+                                    <i class="fa-solid fa-tag text-muted"></i>&nbsp; 
+                                    <a href="" class="text-decoration-none text-muted fw-normal">{{ $book->category_info->category_name }}</a>
+                                </span>
+                                <span class="me-2">
+                                    <i class="fa-solid fa-user text-muted"></i>&nbsp;
+                                    <a href="" class="text-decoration-none text-muted fw-normal">{{ $book->author }}</a>
+                                </span>
+                                <span class="me-2">
+                                    <i class="fa-solid fa-calendar text-muted"></i>&nbsp;
+                                    <a href="" class="text-decoration-none text-muted fw-normal">{{ $book->created_at->format("d-m-Y") }}</a>
+                                </span>
+                            </div>
+                            <div class="mt-3">
+                                <p>{{ substr($book->description, 0 , 200)."..." }}</p>
+                            </div>
+                            <div class="d-flex justify-content-between">
+                                <h5 class="text-muted">Rs. {{ $book->price }}</h5>
+                                <div>
+                                    <button class="btn btn-secondary btn-sm px-4">Buy Now</button>
+                                    <button class="btn btn-secondary btn-sm px-4 ms-2">Read More</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                </div>            
+            @endforeach
+        @else
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="text-muted text-center">No Book Found.</h1>
                 </div>
             </div>
-        </div>            
-        @endforeach
+        @endif
+            
 
         <div class="d-flex justify-content-center">
             {{ $books->onEachSide('1')->links('vendor.pagination.simple-bootstrap-4') }}
