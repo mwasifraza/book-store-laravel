979340d332ecfd6c748c60e83926936ade2b29e7
mwasifraza <mwasifraza95@gmail.com>
2023-02-02 15:50:37 +0500

    new book notification with observer

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -2,15 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Notification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\AddBookRequest;
 use App\Http\Requests\UpdateBookRequest;
 use App\Models\Category;
 use App\Models\Book;
 use App\Models\User;
-use App\Notifications\NewBookAdded;
 
 class AdminDashboardController extends Controller
 {
@@ -43,10 +41,6 @@ class AdminDashboardController extends Controller
         if($path = $request->file('cover')->storeAs('public/', $bookname)){
             $request->merge(['cover_photo' => "storage/{$bookname}"]);
             $book = Book::create($request->except('cover'));
-
-            $users = User::where('role', 'user')->get();
-            Notification::send($users, new NewBookAdded($book));
-
             return redirect()->route('admin.books.page', ['msg' => 'new book added']);
         }
     }
---
+++ app/Observers/BookObserver.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\{User, Book};
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\NewBookAdded;
+
+class BookObserver
+{
+    /**
+     * Handle the Book "created" event.
+     *
+     * @param  \App\Models\Book  $book
+     * @return void
+     */
+    public function created(Book $book)
+    {
+        $users = User::where('role', 'user')->whereNotNull('email_verified_at')->get();
+        Notification::send($users, new NewBookAdded($book));
+    }
+
+    /**
+     * Handle the Book "updated" event.
+     *
+     * @param  \App\Models\Book  $book
+     * @return void
+     */
+    public function updated(Book $book)
+    {
+        // pp($book);
+    }
+
+    /**
+     * Handle the Book "deleted" event.
+     *
+     * @param  \App\Models\Book  $book
+     * @return void
+     */
+    public function deleted(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Handle the Book "restored" event.
+     *
+     * @param  \App\Models\Book  $book
+     * @return void
+     */
+    public function restored(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Handle the Book "force deleted" event.
+     *
+     * @param  \App\Models\Book  $book
+     * @return void
+     */
+    public function forceDeleted(Book $book)
+    {
+        //
+    }
+}
